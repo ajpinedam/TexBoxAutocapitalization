1e28a9dc163a816da4c9c5a8f5995565f278ee22
Andres Pineda <pineda.andres@gmail.com>
2022-08-26 12:33:01 -0400

    Add TextBox and change AutoCapitalizationType

--- ValidateTextScope/ViewController.swift
+++ ValidateTextScope/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .green
+        
+        let editText = UITextField(frame: CGRect(x:20, y:80, width:300, height:50))
+        editText.backgroundColor = .white
+        
+        editText.autocapitalizationType = .none
+        
+        editText.placeholder = "Enter your Text"
+        
+        
+        self.view.addSubview(editText)
     }
 
 
